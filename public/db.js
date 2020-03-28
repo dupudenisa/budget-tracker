93335f0979f0701abe32811e06a1dd870093697c
Denisa Dupu <dupudenisa@gmail.com>
2020-03-27 19:07:12 -0500

    dispatching a fetch and posting records found

--- public/db.js
+++ public/db.js
@@ -10,3 +10,46 @@ request.onupgradeneeded = function(event) {
     db = event.target.result;
     db.createObject("pending", { autoIncrement: true});
 };
+
+//running our check db
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.online) {
+        checkdatabase();
+    }
+};
+
+// logging error 
+
+request.onerror = function(event) {
+    console.log(req);
+};
+
+
+function checkdatabase() {
+    db = request.result;
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending")
+    const getAll = store.getAll();
+}
+
+// if we have a successful retrieval we are going to dispatch a fetch & posting all records found:
+
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+          .then(response => response.json())
+          .then(()=>{
+
+          });
+    }
+};
